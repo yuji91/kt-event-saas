e61989a0d7d99dd475561e7784a2f2f6245cc8fa
yuji91 <yuji91.1227@gmail.com>
2025-04-28 23:06:30 +0900

    Jpa版Auditable（監査クラス）を必ず読み込むよう @.EnableJpaAuditing アノテーションを設定した

--- src/main/kotlin/com/example/kteventsaas/KtEventSaasApplication.kt
+++ src/main/kotlin/com/example/kteventsaas/KtEventSaasApplication.kt
@@ -2,8 +2,10 @@ package com.example.kteventsaas
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class KtEventSaasApplication
 
 fun main(args: Array<String>) {
