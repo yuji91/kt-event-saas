d9a4279c9e0290af4bea9634791543455d97c2f2
yuji91 <yuji91.1227@gmail.com>
2025-04-29 21:09:33 +0900

    refactor: 補足コメントの内容をより正確な形にした

--- src/main/kotlin/com/example/kteventsaas/infrastructure/persistence/tenant/TenantSpringDataRepository.kt
+++ src/main/kotlin/com/example/kteventsaas/infrastructure/persistence/tenant/TenantSpringDataRepository.kt
@@ -7,11 +7,17 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 /**
- * Spring Data JPA 用インタフェース
- * JpaRepository でカバーできる標準メソッド以外の内容を定義する
+ * Spring Data JPA 用インタフェース（JpaRepository の標準メソッド以外を定義）
  *
- * findByName ではドメイン VO の value (String) を使うように定義しておくのが一般的だが、
- * Spring Data JPA の自動クエリ生成の仕組み上、Spring内部で型不一致エラーになるためVOで定義
+ * ※ Spring Data JPAの自動クエリ生成では、
+ * Repositoryメソッドの引数型と、エンティティプロパティ型が完全一致していることが前提となる。
+ *
+ * たとえエンティティ側で @Converter を設定していても、
+ * Repositoryメソッドのパラメータ解析時には無視されるため、
+ * findBy系メソッドの引数には、エンティティのプロパティ型（ここではTenantName）をそのまま使う必要がある。
+ *
+ * そのため、VOのvalue（String）ではなく、VO型（TenantName）で定義する。
+ * (@Queryアノテーションで別途JPQLクエリを記載すれば回避可能)
  */
 interface TenantSpringDataRepository : JpaRepository<TenantJpaEntity, UUID> {
     fun findByName(name: TenantName): TenantJpaEntity?
