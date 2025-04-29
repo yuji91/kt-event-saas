44d70b0e518485a8810584f95dc6accd1a743bbf
yuji91 <yuji91.1227@gmail.com>
2025-04-29 12:36:46 +0900

    build時に AuditableJpa で追加するカラムが TenantMapper の警告で出ないよう設定を変更

--- src/main/kotlin/com/example/kteventsaas/infrastructure/persistence/tenant/mapper/TenantMapper.kt
+++ src/main/kotlin/com/example/kteventsaas/infrastructure/persistence/tenant/mapper/TenantMapper.kt
@@ -3,8 +3,12 @@ package com.example.kteventsaas.infrastructure.persistence.tenant.mapper
 import com.example.kteventsaas.domain.tenant.entity.Tenant
 import com.example.kteventsaas.infrastructure.persistence.tenant.entity.TenantJpaEntity
 import org.mapstruct.Mapper
+import org.mapstruct.ReportingPolicy
 
-@Mapper(componentModel = "spring")
+@Mapper(
+    componentModel = "spring",
+    unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 interface TenantMapper {
 
     fun toEntity(domain: Tenant): TenantJpaEntity
